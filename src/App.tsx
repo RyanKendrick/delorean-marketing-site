a495d953e6ba149ae2c2bd90cbe5d92ad8e99ee8
RyanKendrick <rpkendrick@gmail.com>
2022-09-12 12:56:08 -0400

    added video background

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div>
       <Header />
+      {/* <video width="100%" autoPlay muted loop>
+        <source 
+          src="https://delorean.com/wp-content/uploads/2022/08/Untitled.mp4" 
+          type="video/mp4"
+          />
+        Your browser does not support the video tag.
+      </video> */}
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
         <div className='header-logo-container'><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
         <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-300x40.png" alt="" /></div>
         <div className='right-div'>
+            <div className='right-div-blank'></div>
             <img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" />
             <div className='hamburger'>
                 <div className='ham-line'></div>
