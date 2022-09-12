a714b373ff96fa1d5f385bb3b6cecc643dbde0b2
RyanKendrick <rpkendrick@gmail.com>
2022-09-12 16:31:29 -0400

    responsive header progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 
-const Header = () => {
+const Header: FunctionComponent = () => {
   return (
-    <div className='header-container'>
-        <div className='header-logo-container'><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
-        <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-300x40.png" alt="" /></div>
-        <div className='right-div'>
-            <div className='right-div-blank'></div>
-            <img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" />
-            <div className='hamburger'>
-                <div className='ham-line'></div>
-                <div className='ham-line'></div>
-                <div className='ham-line'></div>
+    <>
+        <div className='header'>
+            <div className="header-container">
+                <div className="left-div">
+                    <div className='header-logo-container'><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
+                    <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+                    <div className="left-div-blank"></div>
+                </div>
+                <div className='right-div'>
+                    <div className='right-div-blank'></div>
+                    <img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" />
+                    <div className='hamburger'>
+                        <div className='ham-line'></div>
+                        <div className='ham-line'></div>
+                        <div className='ham-line'></div>
+                    </div>
+                </div>
+            </div>
+            <div className="mobile-container">
+            <img className='reserve-button-mobile' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" />
             </div>
         </div>
-       
-    </div>
+    </>
   )
 }
 
 export default Header
+
+
