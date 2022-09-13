136449c99c70dcfe87112f00e63f0fd7e73000fe
RyanKendrick <rpkendrick@gmail.com>
2022-09-13 11:58:36 -0400

    progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,25 +3,27 @@ import React, { FunctionComponent } from 'react'
 const Header: FunctionComponent = () => {
   return (
     <>
-        <div className="header">
-            <div className="left-div">
-                <div><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
+        <div className="header-container">
+            <div className="header">
+                <div className="left-div">
+                    <div><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
+                </div>
+                <div className="center-div">
+                    <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+                </div>
+                <div className="right-div">
+                    <div><img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
+                <div className='hamburger'>
+                     <div className='ham-line'></div>
+                     <div className='ham-line'></div>
+                     <div className='ham-line'></div>
+                 </div>
+                </div>
             </div>
-            <div className="center-div">
-                <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+            <div className="sub-header">
+            <div className='dmc-button-container'><img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
+            <div className='reserve-button-container'><div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div></div>
             </div>
-            <div className="right-div">
-                <div><img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
-            <div className='hamburger'>
-                 <div className='ham-line'></div>
-                 <div className='ham-line'></div>
-                 <div className='ham-line'></div>
-             </div>
-            </div>
-        </div>
-        <div className="sub-header">
-        <div><img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
-        <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
         </div>
     </>
   )
