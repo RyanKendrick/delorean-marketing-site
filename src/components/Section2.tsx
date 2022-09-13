6b28c4c632b6e71e37eebed3315aad6d1f2691e1
RyanKendrick <rpkendrick@gmail.com>
2022-09-13 17:11:07 -0400

    section2 progress

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Section2 = () => {
   return (
     <div className='section2'>
-        <div><img className='experience-btn' src="https://delorean.com/wp-content/uploads/2022/05/Experience-Alpha5@2x.png" alt="" /></div>
+        <img className='background-img' src="https://delorean.com/wp-content/uploads/2022/05/DAL500012-BANNER2-2048x1153.webp" alt="" />
+        
+       <img className='experience-btn' src="https://delorean.com/wp-content/uploads/2022/05/Experience-Alpha5@2x.png" alt="" />
     </div>
   )
 }
