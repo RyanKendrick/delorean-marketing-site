aff73526cee789ebb1d8cb4371c3db62105e94c6
RyanKendrick <rpkendrick@gmail.com>
2022-09-13 10:53:27 -0400

    progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,26 +3,20 @@ import React, { FunctionComponent } from 'react'
 const Header: FunctionComponent = () => {
   return (
     <>
-        <div className='header'>
-            <div className="header-container">
-                <div className="left-div">
-                    <div className='header-logo-container'><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
-                    <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
-                    <div className="left-div-blank"></div>
-                </div>
-                <div className='right-div'>
-                    <div className='right-div-blank'></div>
-                    <img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" />
-                    <div className='hamburger'>
-                        <div className='ham-line'></div>
-                        <div className='ham-line'></div>
-                        <div className='ham-line'></div>
-                    </div>
-                </div>
+        <div className="header">
+            <div className="left-div">
+                <div><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
             </div>
-            <div className="mobile-container">
-                <div><img className='mobile-dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
-                <div><img className='reserve-button-mobile' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+            <div className="center-div">
+                <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+            </div>
+            <div className="right-div">
+                <div><img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
+            <div className='hamburger'>
+                 <div className='ham-line'></div>
+                 <div className='ham-line'></div>
+                 <div className='ham-line'></div>
+             </div>
             </div>
         </div>
     </>
@@ -31,4 +25,25 @@ const Header: FunctionComponent = () => {
 
 export default Header
 
-
+// {/* <div className='header'>
+//     <div className="header-container">
+//         <div className="left-div">
+//             <div className='header-logo-container'><img className='header-logo' src="https://delorean.com/wp-content/uploads/2022/03/DeLorean-Logo-White-2048x75.webp" alt="" /></div>
+//             <div><img className='reserve-button' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+//             <div className="left-div-blank"></div>
+//         </div>
+//         <div className='right-div'>
+//             {/* <div className='right-div-blank'></div> */}
+//             <img className='dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" />
+//             <div className='hamburger'>
+//                 <div className='ham-line'></div>
+//                 <div className='ham-line'></div>
+//                 <div className='ham-line'></div>
+//             </div>
+//         </div>
+//     </div>
+//     <div className="mobile-container">
+//         <div><img className='mobile-dmc-button' src="https://delorean.com/wp-content/uploads/2022/03/DMC-CLASSIC-LOGO-WHITE@2x.png" alt="" /></div>
+//         <div><img className='reserve-button-mobile' src="https://delorean.com/wp-content/uploads/2022/03/Reserve-Explore@3x-1024x138.png" alt="" /></div>
+//     </div>
+// </div> */}
